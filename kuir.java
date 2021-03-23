d3be8263d7e90521da0463d04f7149b97be3caff
wonniiii <eve652358@gmail.com>
2021-03-24 00:32:05 +0900

    3주차 실습

---
+++ kuir.java
@@ -0,0 +1,33 @@
+package week2;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+
+public class kuir {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+
+	
+		makeCollection mc = new makeCollection();
+		makeKeyword mk = new makeKeyword();
+
+		
+
+
+	
+			
+	}
+		
+
+	}
+
+
+
--- makeKeyword.java
+++ makeKeyword.java
@@ -1,10 +1,8 @@
-
 package week2;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,39 +15,38 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.jsoup.Jsoup;
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.jsoup.Jsoup;
-import org.jsoup.parser.*;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-public class HW1 {
-
-	public static void main(String[] args) throws IOException, TransformerException, ParserConfigurationException {
+public class makeKeyword {
+	public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException  {
 		// TODO Auto-generated method stub
 
-	
-		File path = new File("src/2주차 실습 html");
+		File path = new File("src/2���� �ǽ� html");
 		File[] files = path.listFiles();
 		int fileNum = files.length;
-		
 
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
 		Document document = docBuilder.newDocument();
 
 		document.setXmlStandalone(true);
-		
-	
 
 		Element docs = document.createElement("docs");
 		document.appendChild(docs);
-
+	
 		for (int i = 0; i < fileNum; i++) {
-			
+
 			String fileName = files[i].getName();
-			int pos = fileName .lastIndexOf(".");
-			String _fileName= fileName.substring(0,pos);
+			int pos = fileName.lastIndexOf(".");
+			String _fileName = fileName.substring(0, pos);
 
 			String number = Integer.toString(i);
 
@@ -57,41 +54,43 @@ public class HW1 {
 			docs.appendChild(doc);
 
 			doc.setAttribute("id", number);
-			
 
 			Element title = document.createElement("title");
 			title.appendChild(document.createTextNode(_fileName));
 			doc.appendChild(title);
 
 			org.jsoup.nodes.Document origin = Jsoup.parse(files[i], "UTF-8");
-			
-			org.jsoup.nodes.Element bodyText = origin.body();
-			
-			
-			Element body = document.createElement("body");
-						
 
-			body.appendChild(document.createTextNode(bodyText.text()));
+			org.jsoup.nodes.Element bodyText = origin.body();
+
+			Element body = document.createElement("body");
+
 			
+
 			doc.appendChild(body);
+
+			KeywordExtractor ke = new KeywordExtractor();
+			KeywordList kl = ke.extractKeyword(bodyText.text(), true);
+			for (int k = 0; k < kl.size(); k++) {
+				Keyword kwrd = kl.get(k);
+				body.appendChild(document.createTextNode("#" + kwrd.getString() + kwrd.getCnt()));
+
+			}
 
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 
 			Transformer transformer = transformerFactory.newTransformer();
-			
-			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4"); //정렬 스페이스4칸
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes"); //들여쓰기
-            transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes"); //doc.setXmlStandalone(true); 했을때 붙어서 출력되는부분 개행
-			
+
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes");
 
 			DOMSource source = new DOMSource(document);
-			StreamResult result = new StreamResult(new FileOutputStream(new File("src/data/Collection.xml")));
+			StreamResult result = new StreamResult(new FileOutputStream(new File("src/data/index.xml")));
 
 			transformer.transform(source, result);
 
 		}
-
 	}
-
 }
