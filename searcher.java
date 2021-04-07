6b29a116d87504ad8182b276c4bac6be196c6a56
wonniiii <81157265+wonniiii@users.noreply.github.com>
2021-04-07 16:02:57 +0900

    Update searcher.java

--- searcher.java
+++ searcher.java
@@ -19,73 +19,25 @@ import org.xml.sax.SAXException;
 public class searcher {
 
 	void get(String filePath, String query) throws Exception {
-		String[] food = {"¶±","¶ó¸é","¾ÆÀÌ½ºÅ©¸²","ÃÊ¹ä","ÆÄ½ºÅ¸"};
-		File file = new File(filePath+"\\index.post");
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		File file = new File("index.post");
+		String[] food = {"ë–¡","ë¼ë©´","ì•„ì´ìŠ¤í¬ë¦¼","ì´ˆë°¥","íŒŒìŠ¤íƒ€"};
+		
+		//í˜•íƒœì†Œ ë¶„ì„ê¸°
 		KeywordExtractor ke = new KeywordExtractor();
-		KeywordList kl = ke.extractKeyword(query, true);
+		KeywordList kl = ke.extractKeyword(query, true); 
 		for (int j = 0; j < kl.size(); j++) {
 			Keyword kwrd = kl.get(j);
 			map.put(kwrd.getString(), 1);
 		}
+		
+		
+		//hashmap
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		HashMap hashMap = readPost(filePath);
-		Iterator<String> it = hashMap.keySet().iterator();
-		double[] id = new double[5];
-		while(it.hasNext()) {
-			String key = it.next();
-			ArrayList<String> value = (ArrayList<String>)hashMap.get(key);
-			int[] wq = new int[5];
-			double[] q = new double[5];
-			if(map.containsKey(key)) {
-				for (int i = 0; i < 5; i++) {
-					if(value.contains(Integer.toString(i)))
-					{
-						wq[i] = map.get(key);
-						int idx = value.indexOf(Integer.toString(i));
-						q[i] = Double.parseDouble(value.get(idx+1));
-						id[i] += wq[i]*q[i];
-					}
-						
-				}
-			}
-			
-		}
-		for (int i = 0; i < 5; i++) {
-			id[i] = Math.round(id[i]*100)/100.0;
-			System.out.println(id[i]);
-		}
-		double[] title = new double[5];
-		for (int i = 0; i < title.length; i++) {
-			title[i] = id[i];
-		}
-		Arrays.sort(title);
-		String[] top3 = new String[3];
-		int a = 0;
-		for (int i = 4; i >= 0; i--) {
-			for (int j = 0; j < 5; j++) {
-				if(a==3)
-					break;
-				if(title[i]==id[j]) {
-					if(!food[j].equals("")) {
-						top3[a] = food[j];
-						food[j] = "";
-						if(a<=2) {
-							a++;}
-						}
-					}
-				
-			}
-			if(a==3)
-				break;
-		}
-		for (int i = 0; i < top3.length; i++) {
-			System.out.println(top3[i]);
-		}
-	}
-	
-
-	HashMap readPost(String fileName) throws Exception {
-		FileInputStream fileStream = new FileInputStream(fileName+"\\index.post");
+		Iterator<String> keyset = hashMap.keySet().iterator();
+		
+		HashMap readPost(String fileName) throws Exception {
+		FileInputStream fileStream = new FileInputStream("index.post");
 		ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
 
 		Object object = objectInputStream.readObject();
@@ -94,5 +46,59 @@ public class searcher {
 		HashMap hashMap = (HashMap)object;
 		return hashMap;
 
+		
+		double[] name = new double[5];
+		
+		while(it.hasNext()) {
+			String key = keyset.next();
+			ArrayList<String> value = (ArrayList<String>)hashMap.get(key);
+			int[] p = new int[5];
+			double[] h  = new double[5];
+			if(map.containsKey(key)) {
+				for (int i = 0; i < 5; i++) {
+					if(value.contains(Integer.toString(i)))
+					{
+						p[i] = map.get(key);
+						int idx = value.indexOf(Integer.toString(i));
+						h[i] = Double.parseDouble(value.get(idx+1));
+						name[i] += p[i]*h[i];
+					}		
+				}
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			name[i] = Math.round(name[i]*100)/100.0;
+			System.out.println(name[i]);
+		}
+		double[] title = new double[5];
+		for (int i = 0; i < title.length; i++) {
+			title[i] = name[i];
+		}
+		Arrays.sort(title);
+		String[] top = new String[3];
+		int a = 0;
+		for (int i = 4; i >= 0; i--) {
+			for (int j = 0; j < 5; j++) {
+				if(a==3)
+					break;
+				if(title[i]==id[j]) {
+					if(!food[j].equals("")) {
+						top[a] = food[j];
+						food[j] = "";
+						if(a<=2) {
+							a++;}
+						}
+					}
+			}
+			if(a==3)
+				break;
+		}
+		for (int i = 0; i < top.length; i++) {
+			System.out.println(top[i]);
+		}
+	}
+	
+
+	
     }
 }
