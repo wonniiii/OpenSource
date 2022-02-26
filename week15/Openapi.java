2fcd91145f52545ec0b3bdc23eb62885409fff2b
wonniiii <81157265+wonniiii@users.noreply.github.com>
2022-02-26 15:23:59 +0900

    Rename Openapi.java to week15/Openapi.java

--- week15/Openapi.java
+++ week15/Openapi.java
@@ -17,21 +17,21 @@ import org.json.simple.parser.ParseException;
 public class Openapi {
 
     public static void main(String[] args) throws ParseException  {
-    	String clientId = "rE2SHK219EUrnUYGDwa4"; //¾ÖÇÃ¸®ÄÉÀÌ¼Ç Å¬¶óÀÌ¾ğÆ® ¾ÆÀÌµğ°ª"
-        String clientSecret = "SlMVayfhL7"; //¾ÖÇÃ¸®ÄÉÀÌ¼Ç Å¬¶óÀÌ¾ğÆ® ½ÃÅ©¸´°ª"
+    	String clientId = "rE2SHK219EUrnUYGDwa4"; //ì• í”Œë¦¬ì¼€ì´ì…˜ í´ë¼ì´ì–¸íŠ¸ ì•„ì´ë””ê°’"
+        String clientSecret = "SlMVayfhL7"; //ì• í”Œë¦¬ì¼€ì´ì…˜ í´ë¼ì´ì–¸íŠ¸ ì‹œí¬ë¦¿ê°’"
         Scanner scan = new Scanner(System.in);
-        System.out.print("°Ë»ö¾î¸¦ ÀÔ·ÂÇÏ¼¼¿ä:");
+        System.out.print("ê²€ìƒ‰ì–´ë¥¼ ì…ë ¥í•˜ì„¸ìš”:");
         String name=scan.next();
         String text = null;
         try {
        
             text = URLEncoder.encode(name, "UTF-8");
         } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("°Ë»ö¾î ÀÎÄÚµù ½ÇÆĞ",e);
+            throw new RuntimeException("ê²€ìƒ‰ì–´ ì¸ì½”ë”© ì‹¤íŒ¨",e);
         }
 
-        String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + text;    // json °á°ú
-        //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml °á°ú
+        String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + text;    // json ê²°ê³¼
+        //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml ê²°ê³¼
 
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("X-Naver-Client-Id", clientId);
@@ -62,13 +62,13 @@ public class Openapi {
             }
 
             int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // Á¤»ó È£Ãâ
+            if (responseCode == HttpURLConnection.HTTP_OK) { // ì •ìƒ í˜¸ì¶œ
                 return readBody(con.getInputStream());
-            } else { // ¿¡·¯ ¹ß»ı
+            } else { // ì—ëŸ¬ ë°œìƒ
                 return readBody(con.getErrorStream());
             }
         } catch (IOException e) {
-            throw new RuntimeException("API ¿äÃ»°ú ÀÀ´ä ½ÇÆĞ", e);
+            throw new RuntimeException("API ìš”ì²­ê³¼ ì‘ë‹µ ì‹¤íŒ¨", e);
         } finally {
             con.disconnect();
         }
@@ -79,9 +79,9 @@ public class Openapi {
             URL url = new URL(apiUrl);
             return (HttpURLConnection)url.openConnection();
         } catch (MalformedURLException e) {
-            throw new RuntimeException("API URLÀÌ Àß¸øµÇ¾ú½À´Ï´Ù. : " + apiUrl, e);
+            throw new RuntimeException("API URLì´ ì˜ëª»ë˜ì—ˆìŠµë‹ˆë‹¤. : " + apiUrl, e);
         } catch (IOException e) {
-            throw new RuntimeException("¿¬°áÀÌ ½ÇÆĞÇß½À´Ï´Ù. : " + apiUrl, e);
+            throw new RuntimeException("ì—°ê²°ì´ ì‹¤íŒ¨í–ˆìŠµë‹ˆë‹¤. : " + apiUrl, e);
         }
     }
 
@@ -98,7 +98,7 @@ public class Openapi {
 
             return responseBody.toString();
         } catch (IOException e) {
-            throw new RuntimeException("API ÀÀ´äÀ» ÀĞ´Âµ¥ ½ÇÆĞÇß½À´Ï´Ù.", e);
+            throw new RuntimeException("API ì‘ë‹µì„ ì½ëŠ”ë° ì‹¤íŒ¨í–ˆìŠµë‹ˆë‹¤.", e);
         }
     
     }
